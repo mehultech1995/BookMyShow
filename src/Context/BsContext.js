0f16d4842726d174aa72ef2c903268d9774a5a99
mehultech1995 <mehulyadav995@gmail.com>
2023-09-17 19:14:49 +0530

    Updates

--- src/Context/BsContext.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-
-// Creating a new context as BsContext.
-const BsContext = createContext();
-
-export default BsContext;
--- src/data.js
+++
@@ -1,6 +0,0 @@
-
-export let moviesList = ['Jawan', 'The NUN ||', 'Dream Girl 2', 'Gadar 2: The Katha Continues', 'OMG 2', 'Oppenheimer', 'Jailer', 'Rocky Aur Rani Kii Prem Kahaani', 'A Haunting in Venice', 'Smugglers'];
-export let slots = ['10:00 AM', '12:30 PM', '3:00 PM', "5:30 PM", "8:00 PM", "11:30 PM"];
-export let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2'];
-
-
